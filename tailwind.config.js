30c56808fbc94c9cb07b856623d1758d5a9a7cfe
Emre Kayık <54414075+emrekayik@users.noreply.github.com>
2023-08-16 02:10:23 +0000

    v1.0 update

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.md',
+    './*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require("daisyui"),
+    require('@tailwindcss/typography'),
+  ],
+}
